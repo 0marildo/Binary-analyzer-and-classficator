6b4ef03c12f8e88f39d8a095eea6dd94f9b9c593
0marildo <hamza.elnajmi@gmail.com>
2026-02-27 16:44:25 -0300

    CORS atualizado

--- backend/main.py
+++ backend/main.py
@@ -8,7 +8,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
-        "http://https://binary-analyzer-and-classficator-yr.vercel.app"
+        "https://binary-analyzer-and-classficator-yr.vercel.app"
     ],
     allow_methods=["*"],
     allow_headers=["*"],
