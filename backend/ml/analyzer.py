a3e295a334222e40fb31a0cab2e601d3cfa23f64
0marildo <hamza.elnajmi@gmail.com>
2026-03-13 10:34:10 -0300

    now a more optimal solution

--- backend/ml/analyzer.py
+++ backend/ml/analyzer.py
@@ -6,27 +6,23 @@ from .plot import cluster_plot
 def analyze_binary(data: bytes, block_size=32):
     arr = np.frombuffer(data, dtype=np.uint8)
 
-    blocks = [
-        arr[i:i+block_size]
-        for i in range(0, len(arr), block_size)
-        if len(arr[i:i+block_size]) == block_size
-    ]
+
+    n_blocks = len(arr) // block_size
+    blocks = arr[:n_blocks * block_size].reshape(n_blocks, block_size)
 
     features = np.array([
         [entropy_block(b), byte_zero_ratio(b), unique_byte_ratio(b), variance(b)]
         for b in blocks
     ])
 
-    clusters, scores = cluster_blocks(features)
+    clusters = cluster_blocks(features)
     plot = cluster_plot(features, clusters)
     
 
 
     return {
         "num_blocks": len(blocks),
-        "clusters": clusters.tolist(),
-        "silhouette_score": scores,
-        "features": features.tolist(),
+        "clusters": clusters.tolist(),        
         "plot": plot,
         
     }
--- backend/schemas/analyze.py
+++ backend/schemas/analyze.py
@@ -1,11 +1,8 @@
 from pydantic import BaseModel
-from typing import List, Optional
+from typing import List
 
 class AnalyseResponse(BaseModel):
     num_blocks: int
     clusters: List[int]
-    features: List[List[float]]
     plot: str #string em base64
     summary: dict #quantos blocos são headers, raw data, offset
-    silhouette_score: Optional[float] 
-    
